6d4589792aa591c35cd25786a194ad52fedc3f03
ZEAL <tassadiqshakeel5@gmail.com>
2023-04-14 00:01:25 +0500

    pagination

--- Journal/trades/views.py
+++ Journal/trades/views.py
@@ -3,15 +3,17 @@ from django.contrib import messages
 from .models import Trade
 from .forms import TradeForm
 # Create your views here.
-# Dashboard imports
-from django.http import JsonResponse
-# from .models import Order
-from django.core import serializers
-
+from django.core.paginator import Paginator
 
 def index(request):
-    trades = Trade.objects.order_by('entry_time')[:10]
-    context = { 'trades' : trades}
+    trades = Trade.objects.order_by('entry_time')
+    paginator = Paginator(trades,per_page=10)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    print(page_obj.has_next)
+    context = { 'trades' : trades,
+               'page_obj': page_obj}
+
 
     if request.method == 'POST':
         form = TradeForm(request.POST, request.FILES)
