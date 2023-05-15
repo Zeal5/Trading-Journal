0295a68df42c16f5a1089434506ff4f73aaf6c1a
ZEAL <tassadiqshakeel5@gmail.com>
2023-05-15 20:58:35 +0500

    restart

--- Journal/trades/urls.py
+++ Journal/trades/urls.py
@@ -6,9 +6,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('',views.index, name='index'),
 
-
-    # path("",views.add_new_trade, name="update_trade")
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-
-
+]
--- Journal/trades/views.py
+++ Journal/trades/views.py
@@ -5,32 +5,3 @@ from .forms import TradeForm
 # Create your views here.
 from django.core.paginator import Paginator
 from django.views.decorators.csrf import csrf_exempt
-
-@csrf_exempt
-def index(request):
-    trades = Trade.objects.order_by('entry_time')
-    paginator = Paginator(trades, 15)
-    page_number = request.GET.get('page')
-    page_obj = paginator.get_page(page_number)
-
-    context = { 'trades' : page_obj,
-               'page_obj': page_obj}
-
-
-    if request.method == 'POST':
-        form = TradeForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Trade created successfully.')
-            return redirect('index')
-            # do something with the created trade object
-        
-        return redirect('index')
-    else:
-        form = TradeForm()
-
-    context['form'] = form
-    return render(request, 'dashboard.html',context)
-
-
-
