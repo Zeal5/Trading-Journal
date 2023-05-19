70249d6830eb403377dc0685a311ce22d414df69
ZEAL <tassadiqshakeel5@gmail.com>
2023-05-19 22:21:22 +0500

    adding form using vanilla js

--- Journal/trades/static/js/_trades_table.js
+++ Journal/trades/static/js/_trades_table.js
@@ -12,29 +12,86 @@ function toggle_comments(row) {
 
 
 }
-// document.getElementById("tradeForm").addEventListener("submit", function(event) {
-//   event.preventDefault(); // Prevent default form submission
+document.getElementById("tradeForm").addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission
+    const tbody = document.querySelector(".entries-table-body")
 
-//   // Get trade data from input fields
-//   var entry = document.querySelector('input[name="entry"]').value;
-//   var exit = document.querySelector('input[name="exit"]').value;
-//   var dateTime = document.querySelector('input[name="date_time"]').value;
 
-//   // Create trade object
-//   var trade = {
-//     entry: entry,
-//     exit: exit,
-//     date_time: dateTime
-//   };
 
-//   // Send trade data via AJAX
-//   axios.post("/home_page/", trade)
-//     .then(function(response) {
-//       // Handle success response
-//       console.log("Trade uploaded successfully");
-//     })
-//     .catch(function(error) {
-//       // Handle error response
-//       console.error("Error uploading trade:", error);
-//     });
-// });
+    var entry = document.querySelector('input[name="entry_price"]').value;
+    var exit = document.querySelector('input[name="exit_price"]').value;
+    var ticker = document.querySelector('input[name="ticker"]').value;
+    var side = document.querySelector('input[name="side"]').value;
+    var stop_loss = document.querySelector('input[name="stop_loss"]').value;
+    var take_profit = document.querySelector('input[name="take_profit"]').value;
+    var pnl = document.querySelector('input[name="pnl"]').value;
+    var rr = document.querySelector('input[name="rr"]').value;
+    var tags = document.querySelector('input[name="tags"]').value;
+    // var images = document.querySelector('input[name="images"]').value;
+    var comment = document.querySelector('input[name="comment"]').value;
+
+    // adding new row
+    const newRow = document.createElement('tr');
+    newRow.className = 'table-rows';
+    newRow.onclick = function () {
+        toggle_comments(this)
+    }
+
+    // Create table data cells and populate them with"data"
+    const td1 = document.createElement('td');
+    td1.textContent = "data";
+
+    const td2 = document.createElement('td');
+    td2.textContent = "data";
+
+    const td3 = document.createElement('td');
+    td3.textContent = "data";
+
+    const td4 = document.createElement('td');
+    td4.textContent = "data";
+
+    const td5 = document.createElement('td');
+    td5.textContent = "data";
+
+    const td6 = document.createElement('td');
+    td6.textContent = "data";
+
+    const td7 = document.createElement('td');
+    td7.textContent = "data";
+
+    const td8 = document.createElement('td');
+    td8.textContent = "data";
+
+    const td9 = document.createElement('td');
+    td9.textContent = "data";
+
+    const td10 = document.createElement('td');
+    const img = document.createElement('img');
+    img.alt = 'None';
+    img.src = "data";
+    td10.appendChild(img);
+
+    const td11 = document.createElement('td');
+    td11.id = 'comment';
+    td11.textContent = "data dhjfosd jsjdf hjweio fewncindsjfn weonfjsda;f uewnfjds;fh wenfdjas;sd huwendfj wefnhsjfh uwenfju";
+
+    // Append the table data cells to the new row
+    newRow.appendChild(td1);
+    newRow.appendChild(td2);
+    newRow.appendChild(td3);
+    newRow.appendChild(td4);
+    newRow.appendChild(td5);
+    newRow.appendChild(td6);
+    newRow.appendChild(td7);
+    newRow.appendChild(td8);
+    newRow.appendChild(td9);
+    newRow.appendChild(td10);
+    newRow.appendChild(td11);
+
+    const last_child = tbody.lastElementChild;
+    tbody.insertBefore(newRow, last_child)
+
+
+
+});
+
